c8ab103162e43cfd4e1eb2fc9ce08d4ab3c63ac7
Brandon Pouliot <brandonpouliot@gmail.com>
2024-02-06 21:30:04 -0500

    added apikey and all constant elements to javascript file

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    const apiKey = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
+    const searchform = $('#searchForm');
+    const cityInput = $('#cityInput');
+    const searchHistory = $('#searchHistory');
+    const currentWeather = $('#currentWeather');
+    const forecast = $('#forcast');
+})
