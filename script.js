2cddd2b8977d4edcd1d47ef33d9f26f5e36fd2be
Brandon Pouliot <brandonpouliot@gmail.com>
2024-02-07 19:40:56 -0500

    added the function to get weather data from specified cities

--- script.js
+++ script.js
@@ -14,4 +14,33 @@ $(document).ready(function() {
             cityInput.val('');
         }
     });
+    function getWeatherData(city) {
+        cosnt apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+        $.ajax({
+            url:apiUrl,
+            method: 'GET',
+            success: function (data) {
+                displayCurrentWeather(data);
+                addToSearchHistory(city);
+                getForecast(data.coord.let, data.coord.lon);
+            },
+            error: function(error) {
+                console.error('Error fetching weather data:', error);
+            }
+        });
+    }
+    function getForecast(lat, lon) {
+        const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+        $.ajax({
+            url: apiUrl,
+            method: 'GET',
+            success: function (data) {
+                displayForecast(data);
+            },
+            error: function (error) {
+                console.error('Error fetching forcast data:', error);
+            }
+        });
+    }
+    
 })
